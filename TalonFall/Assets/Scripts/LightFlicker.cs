1bd610092be9ba4a63db3a7e2fca501fcf2ebb3c
jedklika <43482414+jedklika@users.noreply.github.com>
2020-04-09 02:02:41 -0400

    Kitchen Fleshed out
    Flickering light prefab added, dinner area fleshed out mostly, going to place the juke box in the kitchen tomorrow

---
+++ TalonFall/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    public GameObject Light;
+    public bool Isflickering = false;
+    public float delay;
+    public float random1;
+    public float random2;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Isflickering == false)
+        {
+            StartCoroutine(FlickerLight());
+        }
+    }
+    IEnumerator FlickerLight()
+    {
+        Isflickering = true;
+        Light.SetActive(false);
+        delay = Random.Range(random1, random2);
+        yield return new WaitForSeconds(delay);
+        Light.SetActive(true);
+        delay = Random.Range(random1, random2);
+        yield return new WaitForSeconds(delay);
+        Isflickering = false;
+
+    }
+}
