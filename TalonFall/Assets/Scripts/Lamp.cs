1248243f225220e78edcb93668c1e210bd0507f0
jedklika <43482414+jedklika@users.noreply.github.com>
2020-04-09 22:44:21 -0400

    Lamp Working
    Requires a DarkZone trigger to activate and LightZone trigger to deactivate

---
+++ TalonFall/Assets/Scripts/Lamp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+    public GameObject Flame;
+    public bool Lit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Flame.SetActive(false);
+        Lit = false;
+    }
+    private void Update()
+    {
+        if (Lit)
+        {
+            Flame.SetActive(true);
+        }
+        if(Lit == false)
+        {
+            Flame.SetActive(false);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("DarkZone"))
+        {
+            Lit = true;
+        }
+        if (collision.gameObject.CompareTag("LightZone"))
+        {
+            Lit = false;
+        }
+    }
+}
